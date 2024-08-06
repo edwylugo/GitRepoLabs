657873890e58b4ce5bb04923962c87c7410edd18
Edwy Lugo <edwylugo@gmail.com>
2024-08-06 00:08:38 -0300

    Create environment Plist

--- GitRepoLabs/GitRepoLabs/Networking/API/URLBase.swift
+++ GitRepoLabs/GitRepoLabs/Networking/API/URLBase.swift
@@ -8,12 +8,25 @@
 import Foundation
 
 struct API {
+    
+    private static let environmentFile = Bundle.main.path(forResource: "environment", ofType: "plist")
+    
     static func baseURL() -> String {
-        return "https://api.github.com/"
+        if let environmentFile = environmentFile {
+            if let environmentDictionary = NSDictionary(contentsOfFile: environmentFile) {
+                return environmentDictionary["BaseUrl"] as? String ?? ""
+            }
+        }
+        return ""
     }
     
     // PERSONAL ACCESS - Expire in 90 days
     static func tokenGit() -> String {
-        return "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"
+        if let environmentFile = environmentFile {
+            if let environmentDictionary = NSDictionary(contentsOfFile: environmentFile) {
+                return environmentDictionary["PersonalToken"] as? String ?? ""
+            }
+        }
+        return ""
     }
 }
